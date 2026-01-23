7c18ee657850bf57f7676d4e77586482d84db2ba
Tsanta <tsantamrindra.dev@gmail.com>
2026-01-24 02:02:48 +0300

    u package 21

--- libs/prisma/src/index.ts
+++ libs/prisma/src/index.ts
@@ -3,6 +3,7 @@ import { CheckGameFinished } from "./functions/check-game-finished.js"
 
 const prisma = new PrismaClient();
 export default prisma;
+export * from '@prisma/client'
 export {
     CheckGameFinished
 }
