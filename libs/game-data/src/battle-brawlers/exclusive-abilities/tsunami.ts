2da73fe0357dd33916bd141e33704b71f3baf0b5
Tsanta <tsantamirindra.dev@gmail.com>
2026-05-02 23:10:37 +0300

    Update Tsunami ability description and enhance activation conditions to prevent use during battles

--- libs/game-data/src/battle-brawlers/exclusive-abilities/tsunami.ts
+++ libs/game-data/src/battle-brawlers/exclusive-abilities/tsunami.ts
@@ -6,7 +6,7 @@ export const Tsunami: exclusiveAbilitiesType = {
     key: 'tsunami',
     name: 'Tsunami Wave',
     maxInDeck: 1,
-    description: `When three allied Aquos Bakugan, including Aquos Siege, are present on the field, this card eliminates all Bakugan on the field except Aquos Siege.`,
+    description: `When three allied Aquos Bakugan, including Aquos Siege, are present on the field, this card eliminates all Bakugan on the field except Aquos Siege. (Can't be used if a battle is in process or paused)`,
     usable_in_neutral: true,
     usable_if_user_not_on_domain: false,
     image: 'tsunami.jpg',
@@ -43,6 +43,8 @@ export const Tsunami: exclusiveAbilitiesType = {
     },
     activationConditions({ roomState, userId }) {
         if (!roomState) return false
+        const {battleInProcess, paused} = roomState.battleState
+        if(battleInProcess || paused) return false
         const bakugans = roomState.protalSlots.map((slot) => slot.bakugans).flat()
         const aquosBakugans = bakugans.filter((bakugan) => bakugan.attribut === "Aquos" && bakugan.userId === userId)
         const siege = aquosBakugans.find((bakugan) => bakugan.key === SiegeAquos.key)
