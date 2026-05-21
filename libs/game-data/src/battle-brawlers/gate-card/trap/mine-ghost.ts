7554cf85ed66e6d735e9c51f87aad2e2fb9f3693
Tsanta <tsantamirindra.dev@gmail.com>
2026-05-21 10:41:41 +0300

    feat: Implement Mine Ghost trap card logic and additional request handling

--- libs/game-data/src/battle-brawlers/gate-card/trap/mine-ghost.ts
+++ libs/game-data/src/battle-brawlers/gate-card/trap/mine-ghost.ts
@@ -1,5 +1,50 @@
-import { AbilityCardsList, AnimationDirectivesTypes, CancelGateCardAbilities, CheckBattle, CheckBattleStillInProcess, ElimineBakuganEffect, ExclusiveAbilitiesList, gateCardAdditionalRequest,  GetUserName, RemoveGateCardDirectiveAnimation, ResetSlot, Slots, type gateCardType } from "../../../index.js";
+import { AbilityCardsList, AnimationDirectivesTypes, CancelGateCardAbilities, CheckBattle, CheckBattleStillInProcess, ElimineBakuganEffect, ExclusiveAbilitiesList, gateCardAdditionalRequest, GetUserName, portalSlotsTypeElement, RemoveGateCardDirectiveAnimation, ResetSlot, resolutionGateCardType, ResolveTrapCardAdditionalRequest, ResolveTrapCardOnOpen, Slots, stateType, type gateCardType } from "../../../index.js";
 import { GateCardImages } from "../../../store/gate-card-images.js";
+
+type MineGhostEffectParams = {
+    roomState: stateType
+    slotOfGate: portalSlotsTypeElement
+    otherPlayerId: string
+}
+
+function MineGhostMainEffect({
+    roomState,
+    slotOfGate,
+    otherPlayerId
+}: MineGhostEffectParams): null | gateCardAdditionalRequest {
+
+    slotOfGate.bakugans.forEach((bakugan) => {
+        ElimineBakuganEffect({
+            bakugan,
+            roomState,
+            gateCardProtection: true
+        })
+    })
+
+    roomState.turnState.turn = slotOfGate.portalCard!.userId
+    roomState.turnState.previous_turn = otherPlayerId
+
+    RemoveGateCardDirectiveAnimation({
+        animations: roomState.animations,
+        slot: slotOfGate,
+        roomState
+    })
+
+    ResetSlot(slotOfGate)
+
+    roomState.turnState.set_new_bakugan = true
+    roomState.turnState.set_new_gate = true
+
+    CheckBattleStillInProcess(roomState)
+
+    CheckBattle({
+        roomState
+    })
+
+    return {
+        type: 'TURN_ACTION_LAUNCHER'
+    }
+}
 
 export const MineFantome: gateCardType = {
     key: 'mine-fantome',
@@ -8,202 +53,76 @@ export const MineFantome: gateCardType = {
     description: `When two ore Bakugan stand on the Card, no matter wich side they are on, they both lose`,
     image: GateCardImages.command,
     onOpen: ({ roomState, slot }) => {
-        const slotOfGate = roomState?.protalSlots.find((s) => s.id === slot)
-        const otherPlayerId = roomState?.players.find((p) => p.userId !== slotOfGate?.portalCard?.userId)?.userId
 
-        if (roomState && slotOfGate && slotOfGate.portalCard !== null && slotOfGate.state.open === false && slotOfGate.state.canceled === false && otherPlayerId) {
-            slotOfGate.state.open = true
-            const bakugans = slotOfGate.bakugans
+        const slotOfGate = roomState?.protalSlots.find(
+            (s) => s.id === slot
+        )
 
-            const opponentsBakugans = bakugans.filter((b) => b.userId !== slotOfGate.portalCard?.userId)
-            const attributs = opponentsBakugans.map((b) => b.attribut)
-            const deck = roomState.decksState.find((d) => d.userId === otherPlayerId)
-            const cards = deck?.abilities.filter((c) => CancelGateCardAbilities.includes(c.key) && !c.used && (c.attribut && attributs.includes(c.attribut)))
+        const otherPlayerId = roomState?.players.find(
+            (p) => p.userId !== slotOfGate?.portalCard?.userId
+        )?.userId
 
-            if (deck && opponentsBakugans.length > 0 && cards && cards.length > 0) {
-
-                const request: gateCardAdditionalRequest = {
-                    type: 'SELECT_ABILITY_CARD',
-                    data: cards.map((card) => ({
-                        key: card.key,
-                        description: card.description,
-                        image: [...AbilityCardsList, ...ExclusiveAbilitiesList].find((ability) => ability.key === card.key)?.image || '',
-                        name: card.name
-                    })),
-                    message: `Mine Ghost : Select one ability card`,
-                    skipable: true,
-                    target: otherPlayerId
-                }
-
-                // console.log('request gate card', request.type)
-
-                return request
-
-            } else {
-                bakugans.forEach((bakugan) => {
-                    ElimineBakuganEffect({ bakugan: bakugan, roomState: roomState, gateCardProtection: true })
-                })
-
-                roomState.turnState.turn = slotOfGate.portalCard.userId
-                roomState.turnState.previous_turn = otherPlayerId
-
-                RemoveGateCardDirectiveAnimation({
-                    animations: roomState.animations,
-                    slot: slotOfGate,
-                    roomState: roomState
-                })
-
-                ResetSlot(slotOfGate)
-                roomState.turnState.set_new_bakugan = true
-                roomState.turnState.set_new_gate = true
-                CheckBattleStillInProcess(roomState)
-                CheckBattle({
-                    roomState: roomState
-                })
-                return {
-                    type: 'TURN_ACTION_LAUNCHER'
-                }
-            }
-
-        } else {
+        if (
+            !roomState ||
+            !slotOfGate ||
+            slotOfGate.portalCard === null ||
+            slotOfGate.state.open === true ||
+            slotOfGate.state.canceled === true ||
+            !otherPlayerId
+        ) {
             return null
         }
 
+        slotOfGate.state.open = true
+
+        return ResolveTrapCardOnOpen({
+            roomState,
+            slotOfGate,
+            otherPlayerId,
+            onNoCard: () => {
+                return MineGhostMainEffect({
+                    roomState,
+                    slotOfGate,
+                    otherPlayerId
+                })
+            }
+        })
     },
     onAdditionalRequest({ resolution, roomState }) {
 
-        const slotOfGate = roomState?.protalSlots[Slots.indexOf(resolution.slot)]
-        const bakugans = slotOfGate.bakugans
-        const opponentsBakugans = bakugans.filter((b) => b.userId !== slotOfGate.portalCard?.userId)[0]
-        const otherPlayerId = roomState?.players.find((p) => p.userId !== slotOfGate?.portalCard?.userId)?.userId
+        const slotOfGate =
+            roomState?.protalSlots[
+            Slots.indexOf(resolution.slot)
+            ]
 
-        if (roomState && slotOfGate && slotOfGate.portalCard !== null && otherPlayerId) {
-            if (resolution.data.type !== 'SELECT_ABILITY_CARD') {
+        const otherPlayerId = roomState?.players.find(
+            (p) => p.userId !== slotOfGate?.portalCard?.userId
+        )?.userId
 
-                bakugans.forEach((bakugan) => {
-                    ElimineBakuganEffect({ bakugan: bakugan, roomState: roomState, gateCardProtection: true })
-                })
-
-                roomState.turnState.turn = slotOfGate.portalCard.userId
-                roomState.turnState.previous_turn = otherPlayerId
-
-                RemoveGateCardDirectiveAnimation({
-                    animations: roomState.animations,
-                    slot: slotOfGate,
-                    roomState
-                })
-
-                ResetSlot(slotOfGate)
-                roomState.turnState.set_new_bakugan = true
-                roomState.turnState.set_new_gate = true
-                CheckBattleStillInProcess(roomState)
-                CheckBattle({
-                    roomState: roomState
-                })
-                return {
-                    type: 'TURN_ACTION_LAUNCHER'
-                }
-
-            } else {
-
-                const { cardOwnerId, card } = resolution.data
-                const abilityCardUsed = roomState.decksState.find((d) => d.userId === cardOwnerId)?.abilities.find((a) => a.key === card.key && a.used === false)
-
-                if (abilityCardUsed) {
-                    // FR: Marquer la carte capacité comme utilisée
-                    // ENG: Mark the ability card as used
-                    abilityCardUsed.used = true
-                }
-
-                const cardData = [...AbilityCardsList, ...ExclusiveAbilitiesList].flat().find((c) => c.key === card.key)
-
-                const activeCardAnimation: AnimationDirectivesTypes = {
-                    type: 'ACTIVE_ABILITY_CARD',
-                    data: {
-                        card: card.key,
-                        attribut: cardData?.attribut ? cardData.attribut : opponentsBakugans.attribut
-                    },
-                    resolve: false,
-                    message: [{
-                        text: `Ability Card Activate : ${card.name}`,
-                        userName: GetUserName({
-                            roomData: roomState,
-                            userId: cardOwnerId,
-                        }),
-                        turn: roomState.turnState.turnCount
-                    }, {
-                        text: `${card.description}`,
-                        turn: roomState.turnState.turnCount,
-                        description: true
-                    }]
-                }
-
-                roomState.animations.push(activeCardAnimation)
-
-                // console.log('card key', cardData?.key)
-                cardData?.onActivate({
-                    roomState: roomState,
-                    roomId: roomState.roomId,
-                    slot: resolution.slot,
-                    userId: opponentsBakugans.userId,
-                    bakuganKey: opponentsBakugans.key
-                })
-
-                // console.log('slot state', slotOfGate.state.canceled)
-                const ownerBakugans = bakugans.filter((b) => b.userId === slotOfGate.portalCard?.userId)
-
-                if (slotOfGate.state.canceled === true) {
-                    if (ownerBakugans.length === 0) {
-                        CheckBattle({
-                            roomState: roomState
-                        })
-                    } else {
-                        RemoveGateCardDirectiveAnimation({
-                            animations: roomState.animations,
-                            slot: slotOfGate,
-                            roomState
-                        })
-
-                        ResetSlot(slotOfGate)
-                        roomState.turnState.set_new_bakugan = true
-                        roomState.turnState.set_new_gate = true
-                        CheckBattleStillInProcess(roomState)
-                        CheckBattle({
-                            roomState: roomState
-                        })
-                    }
-                } else {
-                    bakugans.forEach((bakugan) => {
-                        ElimineBakuganEffect({ bakugan: bakugan, roomState: roomState, gateCardProtection: true })
-                    })
-
-                    roomState.turnState.turn = slotOfGate.portalCard.userId
-                    roomState.turnState.previous_turn = otherPlayerId
-
-                    RemoveGateCardDirectiveAnimation({
-                        animations: roomState.animations,
-                        slot: slotOfGate,
-                        roomState
-                    })
-
-                    ResetSlot(slotOfGate)
-                    roomState.turnState.set_new_bakugan = true
-                    roomState.turnState.set_new_gate = true
-                    CheckBattleStillInProcess(roomState)
-                    CheckBattle({
-                        roomState: roomState
-                    })
-                }
-
-                return {
-                    type: 'TURN_ACTION_LAUNCHER'
-                }
+        if (
+            !roomState ||
+            !slotOfGate ||
+            slotOfGate.portalCard === null ||
+            !otherPlayerId
+        ) {
+            return {
+                type: 'TURN_ACTION_LAUNCHER'
             }
         }
 
-        return {
-            type: 'TURN_ACTION_LAUNCHER'
-        }
+        return ResolveTrapCardAdditionalRequest({
+            resolution,
+            roomState,
+            slotOfGate,
+            otherPlayerId,
+            onMainEffect: () => {
+                return MineGhostMainEffect({
+                    roomState,
+                    slotOfGate,
+                    otherPlayerId
+                })
+            }
+        })
     },
     autoActivationCheck: ({ portalSlot }) => {
         const bakugansOnSlot = portalSlot.bakugans.length
---
+++ libs/game-data/src/function/ability-cards-effects/trap-gate-cards-additional.use-ability.ts
@@ -0,0 +1,179 @@
+import { AbilityCardsList, AnimationDirectivesTypes, CancelGateCardAbilities, CheckBattle, CheckBattleStillInProcess, ExclusiveAbilitiesList, gateCardAdditionalRequest, GetUserName, portalSlotsTypeElement, RemoveGateCardDirectiveAnimation, ResetSlot, resolutionGateCardType, stateType } from "../../index.js";
+
+
+export type ResolveMineGhostOpenParams = {
+    roomState: stateType
+    slotOfGate: portalSlotsTypeElement
+    otherPlayerId: string
+    onNoCard: () => null | gateCardAdditionalRequest
+}
+
+export function ResolveTrapCardOnOpen({
+    roomState,
+    slotOfGate,
+    otherPlayerId,
+    onNoCard
+}: ResolveMineGhostOpenParams): null | gateCardAdditionalRequest {
+
+    const bakugans = slotOfGate.bakugans
+
+    const opponentsBakugans = bakugans.filter(
+        (b) => b.userId !== slotOfGate.portalCard?.userId
+    )
+
+    const attributs = opponentsBakugans.map((b) => b.attribut)
+
+    const deck = roomState.decksState.find(
+        (d) => d.userId === otherPlayerId
+    )
+
+    const cards = deck?.abilities.filter(
+        (c) =>
+            CancelGateCardAbilities.includes(c.key) &&
+            !c.used &&
+            c.attribut &&
+            attributs.includes(c.attribut)
+    )
+
+    if (deck && opponentsBakugans.length > 0 && cards && cards.length > 0) {
+
+        const request: gateCardAdditionalRequest = {
+            type: 'SELECT_ABILITY_CARD',
+            data: cards.map((card) => ({
+                key: card.key,
+                description: card.description,
+                image:
+                    [...AbilityCardsList, ...ExclusiveAbilitiesList]
+                        .find((ability) => ability.key === card.key)
+                        ?.image || '',
+                name: card.name
+            })),
+            message: `Mine Ghost : Select one ability card`,
+            skipable: true,
+            target: otherPlayerId
+        }
+
+        return request
+    }
+
+    return onNoCard()
+}
+
+export type ResolveMineGhostAdditionalRequestParams = {
+    resolution: resolutionGateCardType
+    roomState: stateType
+    slotOfGate: portalSlotsTypeElement
+    otherPlayerId: string
+    onMainEffect: () => null | gateCardAdditionalRequest
+}
+
+export function ResolveTrapCardAdditionalRequest({
+    resolution,
+    roomState,
+    slotOfGate,
+    onMainEffect
+}: ResolveMineGhostAdditionalRequestParams): null | gateCardAdditionalRequest {
+
+    if (resolution.data.type !== 'SELECT_ABILITY_CARD') {
+        return onMainEffect()
+    }
+
+    const bakugans = slotOfGate.bakugans
+
+    const opponentsBakugan = bakugans.find(
+        (b) => b.userId !== slotOfGate.portalCard?.userId
+    )
+
+    if (!opponentsBakugan) {
+        return onMainEffect()
+    }
+
+    const { cardOwnerId, card } = resolution.data
+
+    const abilityCardUsed = roomState.decksState
+        .find((d) => d.userId === cardOwnerId)
+        ?.abilities.find(
+            (a) => a.key === card.key && a.used === false
+        )
+
+    if (abilityCardUsed) {
+        abilityCardUsed.used = true
+    }
+
+    const cardData = [
+        ...AbilityCardsList,
+        ...ExclusiveAbilitiesList
+    ].find((c) => c.key === card.key)
+
+    const activeCardAnimation: AnimationDirectivesTypes = {
+        type: 'ACTIVE_ABILITY_CARD',
+        data: {
+            card: card.key,
+            attribut:
+                cardData?.attribut || opponentsBakugan.attribut
+        },
+        resolve: false,
+        message: [
+            {
+                text: `Ability Card Activate : ${card.name}`,
+                userName: GetUserName({
+                    roomData: roomState,
+                    userId: cardOwnerId
+                }),
+                turn: roomState.turnState.turnCount
+            },
+            {
+                text: `${card.description}`,
+                turn: roomState.turnState.turnCount,
+                description: true
+            }
+        ]
+    }
+
+    roomState.animations.push(activeCardAnimation)
+
+    cardData?.onActivate({
+        roomState,
+        roomId: roomState.roomId,
+        slot: resolution.slot,
+        userId: opponentsBakugan.userId,
+        bakuganKey: opponentsBakugan.key
+    })
+
+    const ownerBakugans = bakugans.filter(
+        (b) => b.userId === slotOfGate.portalCard?.userId
+    )
+
+    if (slotOfGate.state.canceled === true) {
+
+        if (ownerBakugans.length === 0) {
+            CheckBattle({
+                roomState
+            })
+        } else {
+
+            RemoveGateCardDirectiveAnimation({
+                animations: roomState.animations,
+                slot: slotOfGate,
+                roomState
+            })
+
+            ResetSlot(slotOfGate)
+
+            roomState.turnState.set_new_bakugan = true
+            roomState.turnState.set_new_gate = true
+
+            CheckBattleStillInProcess(roomState)
+
+            CheckBattle({
+                roomState
+            })
+        }
+
+        return {
+            type: 'TURN_ACTION_LAUNCHER'
+        }
+    }
+
+    return onMainEffect()
+}
